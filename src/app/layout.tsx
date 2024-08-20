8f3b6bf45170cdf83abc32a8eb2405cdbddfd4ec
Hidemitsu <hidemitsu.hashimoto@gmail.com>
2024-08-20 19:57:53 -0300

    refactor: change providers wrapp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { TabContextProvider } from "@/persistence/tabContext";
+import { ProducerContextProvider } from "@/persistence/producerContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <TabContextProvider>
+          <ProducerContextProvider>
+            {children}
+          </ProducerContextProvider>
+        </TabContextProvider>
+      </body>
     </html>
   );
 }
