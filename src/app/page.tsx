f8d1656b887b42728ffbfdcf7893f44dcb55e6fc
Hidemitsu <hidemitsu.hashimoto@gmail.com>
2024-08-20 20:15:54 -0300

    feat: start tab content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
 'use client'
 
-import { citiesFormat } from "@/business/cities";
-import { ProducerContextProvider } from "@/persistence/producerContext";
-import { TabContextProvider } from "@/persistence/tabContext";
-import { useEffect } from "react";
+import { ActiveTab, useTabContext } from "@/persistence/tabContext";
+import TabButton from "@/presentation/TabButton";
 
 export default function Home() {
-  useEffect(() => {
-    citiesFormat('abre-campo')
-  }, [])
+  const { activeTab, setTab } = useTabContext()
+console.log({activeTab: activeTab === ActiveTab.Dash})
   return (
-    <TabContextProvider>
-      <ProducerContextProvider>
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          
-        </main>
-      </ProducerContextProvider>
-    </TabContextProvider>
+    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
+      <header className="flex gap-4">
+        <TabButton active={activeTab === ActiveTab.Dash} text="Dashboard" onChange={() => setTab?.(ActiveTab.Dash)} />
+        <TabButton active={activeTab === ActiveTab.Form} text="Formulário" onChange={() => setTab?.(ActiveTab.Form)} />
+      </header>
+
+      <section>
+        {activeTab === ActiveTab.Dash ? (
+          <h1>Dash</h1>
+        ) : null}
+        
+        {activeTab === ActiveTab.Form ? (
+          <h1>Form</h1>
+        ) : null}
+      </section>
+    </main>
   );
 }
