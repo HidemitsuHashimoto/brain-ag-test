2563c73017687b0b5e455d3496e9b80a1bff3284
Hidemitsu <hidemitsu.hashimoto@gmail.com>
2024-08-22 19:26:45 -0300

    feat: add citiesDatabaseFormat

--- src/business/cities.ts
+++ src/business/cities.ts
@@ -1,3 +1,7 @@
 export function citiesFormat(cityId: string) {
   return cityId.split('-').map(word => word.charAt(0).toUpperCase() + word.substring(1, word.length)).join(' ')
 }
+
+export function citiesDatabaseFormat(cityId: string) {
+  return cityId.split(' ').map(word => word.toLocaleLowerCase()).join('-')
+}
